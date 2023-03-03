e6a7a6f4e96cc60982dfc218761436d65c8c7edc
EloxZ <45355879+EloxZ@users.noreply.github.com>
2023-03-03 03:29:56 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'KSP2 Modder Knowledge'
+copyright = '2023, EloxZ'
+author = 'EloxZ'
 
 release = '0.1'
 version = '0.1.0'
